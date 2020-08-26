dd44af438f79cacb0f1f60292344642cda1f934a
Rafael Assad <rafaassadeng@gmail.com>
2020-08-26 20:13:36 -0300

    Updates

---
+++ precache-manifest.bf4ff0dc470302b32f45efb005518a62.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82ea7f235698fd283fa782d9233c57e",
+    "url": "/trybetrivia/index.html"
+  },
+  {
+    "revision": "f9e7b1140c155f6e2b4a",
+    "url": "/trybetrivia/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "e632ace42ef5d67edb56",
+    "url": "/trybetrivia/static/css/main.49b3b412.chunk.css"
+  },
+  {
+    "revision": "f9e7b1140c155f6e2b4a",
+    "url": "/trybetrivia/static/js/2.0f4d08d3.chunk.js"
+  },
+  {
+    "revision": "e48c56092a7b59a8a0493c4aaa1b8ec9",
+    "url": "/trybetrivia/static/js/2.0f4d08d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e632ace42ef5d67edb56",
+    "url": "/trybetrivia/static/js/main.1308be07.chunk.js"
+  },
+  {
+    "revision": "13966f38b6bf22765cc8",
+    "url": "/trybetrivia/static/js/runtime-main.83f27215.js"
+  },
+  {
+    "revision": "900f926ce90602d3c10351995f7dd37d",
+    "url": "/trybetrivia/static/media/abertura2.900f926c.mp3"
+  },
+  {
+    "revision": "52a5358a7b16a2b64e4522971d61dd38",
+    "url": "/trybetrivia/static/media/certaResposta.52a5358a.mp3"
+  },
+  {
+    "revision": "af53b9462a53be0b3f50ff8d6b0e0038",
+    "url": "/trybetrivia/static/media/champions.af53b946.mp3"
+  },
+  {
+    "revision": "55a61b6c9188334098944c783ebdd88c",
+    "url": "/trybetrivia/static/media/gif1.55a61b6c.gif"
+  },
+  {
+    "revision": "bca30f5f390e1853c679952545c86e72",
+    "url": "/trybetrivia/static/media/gif2.bca30f5f.gif"
+  },
+  {
+    "revision": "2f1af7e9701ffcaa421fc131fb435b0f",
+    "url": "/trybetrivia/static/media/levelComplete.2f1af7e9.mp3"
+  },
+  {
+    "revision": "3d32ec73fb4e954921acf6b9d038e127",
+    "url": "/trybetrivia/static/media/matrix.3d32ec73.gif"
+  },
+  {
+    "revision": "352bdb3d842d61b77e7d17ec5f101ae7",
+    "url": "/trybetrivia/static/media/quePena.352bdb3d.mp3"
+  },
+  {
+    "revision": "d8470a2e38d15ff22fc4a814639563b4",
+    "url": "/trybetrivia/static/media/seisbraco.d8470a2e.png"
+  },
+  {
+    "revision": "6beee821363a69eef641db93d008e4ef",
+    "url": "/trybetrivia/static/media/suspense.6beee821.mp3"
+  },
+  {
+    "revision": "beb314947d71b49083a4ca1b031ab6dd",
+    "url": "/trybetrivia/static/media/trophy.beb31494.gif"
+  },
+  {
+    "revision": "6432b40a133c0ca5505bed04fb00a353",
+    "url": "/trybetrivia/static/media/underground.6432b40a.mp3"
+  }
+]);
